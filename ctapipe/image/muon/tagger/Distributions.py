be237041a712879f924a6de09541128ed56cd7fc
Roberta Pillera <roberta.pillera@gmail.com>
2019-03-21 18:43:08 +0100

    Debug

--- ctapipe/image/muon/tagger/Distributions.py
+++ ctapipe/image/muon/tagger/Distributions.py
@@ -11,13 +11,23 @@ if __name__ == '__main__':
     print("Program started") 
     if sys.argv[1:]:
         ns = int(sys.argv[1]) #north ns = 1 or south pointing ns=2
-        run = int(sys.argv[2])
+        #run = int(sys.argv[2])
+        group = int(sys.argv[2])
     else:
         ns = 1
+        group = 1
         run = 1
+    """
+    one group processes 500 runs
+    the first job -> group = 1
+    group -> 2 it starts from run 501 up to 1000
+    """
+
+    
 
     sim_dir = "/fefs/aswg/workspace/MC_common/corsika6.9_simtelarray_2018-11-07/LST4_monotrigger/prod3/proton/proton_20190226/"
-    filename = "proton_20deg_"
+    filename = sim_dir+"proton_20deg_"
+    endstring = "___cta-prod3-demo-2147m-LaPalma-baseline-mono.simtel.gz"
     if ns == 1:
         sim_dir += "North_pointing/Data/"
         filename += "0"
@@ -25,35 +35,34 @@ if __name__ == '__main__':
         sim_dir = "South_pointing/Data/"
         filename += "180"
     filename += "deg_run"
-    filename += str(run)
-    filename += "___cta-prod3-demo-2147m-LaPalma-baseline-mono.simtel.gz"
 
-    sim_name = sim_dir + filename
-    n_events = 1
-    tot_nr_ev = 0
-
-    source = event_source(sim_name, max_events=n_events)
-    calib = CameraCalibrator(r1_product="HESSIOR1Calibrator",eventsource=source)
-
-    foutsize_name = "/home/roberta.pillera/MuonAnalysis/ProtonFiles/Run_"+str(run)+"_size.txt"
-    foutsize = open(foutsize_name,"w") #output file
-
-    numev = 0
-    for event in source:
+    start = (group-1)*500+1
+    stop = start + 500
+    for run in range(start,stop):
         
-        print("Event nr %d"%numev)
-        
-        for telid in event.r0.tels_with_data:
-            num_img += 1
-            
-            calib.calibrate(event)
-            size = event.dl1.tel[telid].image[0].sum()
-            foutsize.write(str(size))
-            foutsize.write("\n")
-            print("Size: %f"%size)
-            
-        numev += 1
+        sim_name = filename + str(run) + endstring
+        n_events = 10000
 
+        source = event_source(sim_name, max_events=n_events)
+        calib = CameraCalibrator(r1_product="HESSIOR1Calibrator",eventsource=source)
+
+        foutsize_name = "/home/roberta.pillera/MuonAnalysis/ProtonFiles/Group_"+str(group)+"_size.txt"
+        foutsize = open(foutsize_name,"w") #output file
+
+        numev = 0
+        for event in source:
+            
+            for telid in event.r0.tels_with_data:
+                
+                calib.calibrate(event)
+                size = event.dl1.tel[telid].image[0].sum()
+                foutsize.write(str(size))
+                foutsize.write("\n")
+                
+                
+            numev += 1
+
+    print("%d events in group %d"%(numev,group))
     
     foutsize.close()
 
