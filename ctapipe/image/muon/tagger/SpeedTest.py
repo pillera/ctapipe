604304810cbf10f46c47e774334af44de9b14a7a
Roberta Pillera <roberta.pillera@gmail.com>
2019-03-28 15:19:13 +0100

    debug

--- ctapipe/image/muon/tagger/SpeedTest.py
+++ ctapipe/image/muon/tagger/SpeedTest.py
@@ -84,6 +84,8 @@ if __name__ == '__main__':
             numev += 1
             tot_numev += 1
             if np.array(tag).sum() == 0: # no image is preselected
+                t_end = time.clock() 
+                t.append(t_end - t_start)
                 continue
             else: #analyze
                 muon_evt = analyze_muon_event(event)
